e906a5606c8baceae685b3321e5a69d1b61015c7
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-12-31 00:28:24 +0530

    provided routing and store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-
+import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import todoStore from "./Store/Store";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider>
+      <Provider store={todoStore}>
+        <App />
+      </Provider>
+    </RouterProvider>
   </React.StrictMode>
 );
