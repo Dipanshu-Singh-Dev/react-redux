7033429dcf698a66e8ce70356558412cf6425146
KGS97 <45433869+KGS97@users.noreply.github.com>
2022-12-30 23:58:10 +0530

    deleted counter

--- src/Store/Store.js
+++
@@ -1,5 +0,0 @@
-import { legacy_createStore as createStore } from "redux";
-import CountReducer from "../Reducer/CountReducer";
-
-const countStore = createStore(CountReducer);
-export default countStore;
